081d3aeff6dda328f2fe10f12491be475873cda6
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-12-26 13:59:38 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 2; // Define o número total de páginas
+
+  // Função que atualiza a página atual para a próxima página
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Função que atualiza a página atual para a página anterior
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="page h-screen mx-5 mt-1" style={{ display: currentPage === 1 ? 'block' : 'none' }}>
+            <div className="margin"></div>
+            <p>
+            Oi mozao, tudo bem? 
+            </p>
+        </div>
+        <div className="page h-screen mx-5 mt-1" style={{ display: currentPage === 2 ? 'block' : 'none' }}>
+            <div className="margin"></div>
+            <p>
+            teste 2<br />
+            </p>
+        </div>
+        <div className="mt-5 absolute top-[50%] w-full flex justify-between">
+            <button onClick={handlePreviousPage} disabled={currentPage === 1} className="p-2 ms-1 bg-gray-300 hover:bg-slate-50 rounded transition-all"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#38352e" viewBox="0 0 256 256"><path d="M208,72H128V32a8,8,0,0,0-13.66-5.66l-96,96a8,8,0,0,0,0,11.32l96,96A8,8,0,0,0,128,224V184h80a16,16,0,0,0,16-16V88A16,16,0,0,0,208,72Zm0,96H120a8,8,0,0,0-8,8v28.69L35.31,128,112,51.31V80a8,8,0,0,0,8,8h88Z"></path></svg></button>
+            <button onClick={handleNextPage} disabled={currentPage === totalPages} className="p-2 me-1 bg-gray-300 hover:bg-slate-50 rounded transition-all"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#38352e" viewBox="0 0 256 256"><path d="M237.66,122.34l-96-96A8,8,0,0,0,128,32V72H48A16,16,0,0,0,32,88v80a16,16,0,0,0,16,16h80v40a8,8,0,0,0,13.66,5.66l96-96A8,8,0,0,0,237.66,122.34ZM144,204.69V176a8,8,0,0,0-8-8H48V88h88a8,8,0,0,0,8-8V51.31L220.69,128Z"></path></svg></button>
+        </div>
     </>
   )
 }
 
-export default App
+export default App;
